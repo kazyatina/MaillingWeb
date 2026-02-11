3892e86ade56e7b5e52775590e97e497bba6033f
olga <koa0308@gmail.com>
2026-02-11 23:22:49 +0400

    конфигурации

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path("admin/", admin.site.urls),
-                  path("mail/", include("mail.urls", namespace="mail")),
+                  path("", include("mail.urls", namespace="mail")),
+                  path("users/", include("users.urls", namespace="users")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
